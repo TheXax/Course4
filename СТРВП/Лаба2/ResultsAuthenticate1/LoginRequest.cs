b74cfc9e3f79871cd2ea6de6a6ae294559ac57aa
TheXax <119885135+TheXax@users.noreply.github.com>
2025-10-25 14:03:06 +0300

    Delete СТРВП/Лаба2/ResultsAuthenticate1 directory

--- СТРВП/Лаба2/ResultsAuthenticate1/LoginRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ResultsAuthenticate
-{
-    public class LoginRequest //модель запроса для аутентификации (то, что клиент отправляет при входе)
-    {
-        public string Login { get; set; }
-        public string Password { get; set; }
-    }
-}
