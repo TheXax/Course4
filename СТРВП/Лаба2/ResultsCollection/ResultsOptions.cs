5eda14e1c44cf9fe8746cca1ffd1c9e3183e4f2f
TheXax <119885135+TheXax@users.noreply.github.com>
2025-10-25 14:03:14 +0300

    Delete СТРВП/Лаба2/ResultsCollection directory

--- СТРВП/Лаба2/ResultsCollection/ResultsOptions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ResultsCollection
-{
-    public class ResultsOptions
-    {
-        public string StorePath { get; set; }
-        public string GlobalMutexName { get; set; } = "Global\\BSTU.Results.Collection.Mutex";
-    }
-}
