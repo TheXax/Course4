8df1bad24423c5b5e95cf6890e7956e6f696162b
TheXax <119885135+TheXax@users.noreply.github.com>
2025-10-25 14:03:21 +0300

    Delete СТРВП/Лаба2/ResultsCollection1 directory

--- СТРВП/Лаба2/ResultsCollection1/ResultsOptions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ResultsCollection
-{
-    public class ResultsOptions //отвечает за то, где и как хранится коллекция результатов (RESULTS) в файловой системе
-    {
-        public string StorePath { get; set; } //путь к файлу с коллекцией результатов
-        public string GlobalMutexName { get; set; } = "Global\\BSTU.Results.Collection.Mutex"; //мьютекс для ограничения одновременного доступа к файлу с результатами
-    }
-}
